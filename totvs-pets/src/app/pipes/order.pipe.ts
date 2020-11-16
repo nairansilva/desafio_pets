58e764387df454174c4bed89809a24d8d6c05e2c
Matheus Dias <matheus.mathdias@gmail.com>
2020-11-16 18:14:04 -0300

    Pipes

---
+++ totvs-pets/src/app/pipes/order.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import _ from 'lodash'
+
+@Pipe({
+  name: 'order',
+  pure: false
+})
+export class OrderPipe implements PipeTransform {
+
+  transform(collection: any[], sortColumn: { column, sort}): unknown {
+
+    if(!collection){
+      return collection
+    }
+
+    return _.orderBy(collection,[sortColumn.column],[sortColumn.sort])
+  }
+
+}
